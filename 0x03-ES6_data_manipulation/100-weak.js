de1bc171334f6901a7506646c6aed4f247aa4b9a
Lynette7 <lynettewanjiru7@gmail.com>
2023-01-04 00:22:22 +0300

    Advanced task 0

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,11 +1,12 @@
-export const weakMap = new WeakMap()
-let count = 1
+export const weakMap = new WeakMap();
+const MAX_ENDPOINT_CALLS = 5;
 
-export function queryAPI (endPoint) {
-  weakMap.set(endPoint, count)
-  count += 1
-  const queryCount = weakMap.get(endPoint)
-  if (queryCount >= 5) {
-    throw new Error('Endpoint load is high')
+export function queryAPI(endpoint) {
+  if (!weakMap.has(endpoint)) {
+    weakMap.set(endpoint, 0);
+  }
+  weakMap.set(endpoint, weakMap.get(endpoint) + 1);
+  if (weakMap.get(endpoint) >= MAX_ENDPOINT_CALLS) {
+    throw new Error('Endpoint load is high');
   }
 }
